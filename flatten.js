86295bbee83b1e2a3f8957c5a600aa915bf908d1
Jeremy Buist <jbuistjbuist@gmail.com>
2022-09-20 19:23:12 -0400

    flatten exercise

---
+++ flatten.js
@@ -0,0 +1,18 @@
+
+
+const flatten = function(twoLevelArray) {
+  let oneLevelArray = [];
+  for (let i = 0; i < twoLevelArray.length; i++) {
+    if (!Array.isArray(twoLevelArray[i])) {
+      oneLevelArray.push(twoLevelArray[i]);
+    } else {
+      for (let x = 0; x < twoLevelArray[i].length; x++) {
+        oneLevelArray.push(twoLevelArray[i][x]);
+      }
+    }
+  }
+  return oneLevelArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+
