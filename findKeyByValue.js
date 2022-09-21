ca16a295ca32ac03c5a383c33d918186ab1b2225
Jeremy Buist <jbuistjbuist@gmail.com>
2022-09-21 15:43:27 -0400

    findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙏🏻 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😩 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+  for (let key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  bumblebee: "yellowblack"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "yellowblack"), 'bumblebee');
