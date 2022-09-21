1d33ae29bb462f9cfff8dec93c2e80aec97c2130
Jeremy Buist <jbuistjbuist@gmail.com>
2022-09-21 16:38:35 -0400

    assertObjectsEqual function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,39 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  const eqObjects = function(object1, object2) {
+    const object1Keys = Object.keys(object1);
+    const object2Keys = Object.keys(object2);
+  
+    if (object1Keys.length !== object2Keys.length) {
+      return false;
+    }
+  
+    for (let key of object1Keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { //part to compare arrays
+        if (object1[key].length !== object2[key].length) {
+          return false;
+        }
+        for (let i = 0; i < object1[key].length; i++) {
+          if (object1[key][i] !== object2[key][i]) {
+            return false;
+          }
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  
+  if (eqObjects(actual, expected) === true) {
+    console.log(`🤓 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(` Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
+  }
+};
+
+assertObjectsEqual({one: [1, 2, 3], two: "two"}, {one: [1, 2, 3], two: "two"});
+
+
+
+
