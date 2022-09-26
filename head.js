bff8d9c342843624a207f7b9bd63f3a7b88b2e92
Jeremy Buist <jbuistjbuist@gmail.com>
2022-09-26 14:09:20 -0400

    head to mocha chai

--- head.js
+++ head.js
@@ -2,5 +2,7 @@ const head = function(array) {
   return array[0];
 };
 
+console.log(head(['5']));
+
 module.exports = head;
 
--- test/head.test.js
+++ test/head.test.js
@@ -1,5 +1,17 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const head = require("../head");
 
-assertEqual(head([1 , 2 , 4, 6]), 1);
-assertEqual(head([`one`, 2 , 4, 6]), 'one');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 1 for [1, 2, 3, 4]", () => {
+    assert.strictEqual(head([1, 2, 3, 4]), 1);
+  });
+  it("returns 'one' for [`one`, 2 , 4, 6]", () => {
+    assert.strictEqual(head(['one', 2 , 4, 6]), 'one');
+  });
+});
